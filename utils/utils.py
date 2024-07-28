2ee3159bc63f953115bccb900c49e120b1756c56
putskan <ben.lahav101@gmail.com>
2024-07-28 09:35:39 +0300

    add: action mask raise exception

--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,9 @@ from typing import Any, Optional
 import numpy as np
 
 
-def get_action_mask(observation: Any, info: Any) -> Optional[np.ndarray]:
+def get_action_mask(observation: Any, info: Any) -> np.ndarray:
     if 'action_mask' in info:
         return info['action_mask']
     elif isinstance(observation, dict) and 'action_mask' in observation:
         return observation['action_mask']
-
+    raise ValueError('Could not find action mask')
