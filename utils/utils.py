980b594455a1e3d39ed5af8c0b8f12534710d5ca
putskan <ben.lahav101@gmail.com>
2024-07-28 10:09:12 +0300

    add: docs

--- utils/utils.py
+++ utils/utils.py
@@ -4,6 +4,12 @@ import numpy as np
 
 
 def get_action_mask(observation: Any, info: Any) -> np.ndarray:
+    """
+    return the current valid action mask
+    :param observation: observation received from environment
+    :param info: info received from environment
+    :return: a numpy array representing the mask
+    """
     if 'action_mask' in info:
         return info['action_mask']
     elif isinstance(observation, dict) and 'action_mask' in observation:
