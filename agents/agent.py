9bbbf0e7cc5369e3fd416e3ce701565fb6e3b843
putskan <ben.lahav101@gmail.com>
2024-07-28 09:13:26 +0300

    add: train_update method

--- agents/agent.py
+++ agents/agent.py
@@ -9,7 +9,7 @@ class Agent:
     @abstractmethod
     def play(self, env: AECEnv, obs: Any, curr_agent_str: str, action_mask: Optional[np.ndarray]) -> Any:
         """
-        :param env: the env (useful to get possible actions etc)
+        :param env: the env (useful to get possible actions, etc.)
         :param obs: current observation
         :param curr_agent_str: parameter of env.action_space. index of the current player
         :param action_mask: mask to apply to the actions, in order to get the valid ones
@@ -17,6 +17,15 @@ class Agent:
         :return: the action to play
         """
         pass
+
+    def train_update(self, replay_buffer: Any) -> None:
+        """
+        relevant for agents that train (neural networks).
+        perform a train step/update.
+        should not be called for non-trainable agents
+        :return: None
+        """
+        raise NotImplementedError
 
     def __str__(self) -> str:
         return self.__class__.__name__
