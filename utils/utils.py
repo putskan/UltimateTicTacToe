f60891a7ad725a7820ce1a4f0685020b145d4d07
nir.ravski <nir.ravski@mail.huji.ac.il>
2024-07-30 22:18:59 +0300

    add save and load sunctions for trained agents to utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 import numpy as np
+import pickle
 
 
 def get_action_mask(observation: Any, info: Any) -> np.ndarray:
@@ -15,3 +16,23 @@ def get_action_mask(observation: Any, info: Any) -> np.ndarray:
     elif isinstance(observation, dict) and 'action_mask' in observation:
         return observation['action_mask']
     raise ValueError('Could not find action mask')
+
+
+def save_agent(agent, path):
+    """
+    save the agent to a file
+    :param agent: the agent to save
+    :param path: the path to save the agent to
+    """
+    with open(path, 'wb') as f:
+        pickle.dump(agent, f)
+
+
+def load_agent(path):
+    """
+    load an agent from a file
+    :param path: the path to load the agent from
+    :return: the loaded agent
+    """
+    with open(path, 'rb') as f:
+        return pickle.load(f)
