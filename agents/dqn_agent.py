a716a150053dc4ba5cdaaa7904c89c318393bfdd
putskan <ben.lahav101@gmail.com>
2024-08-30 23:36:52 +0300

    working DQN, better logging

--- agents/dqn_agent.py
+++ agents/dqn_agent.py
@@ -1,10 +1,12 @@
-from typing import Dict, Any
+from typing import Dict, Any, Optional
 
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import random
+
+from torch.optim.lr_scheduler import ExponentialLR
 
 from agents.trainable_agent import TrainableAgent
 from models.dqn import DQN
@@ -13,9 +15,9 @@ import copy
 
 
 class DQNAgent(TrainableAgent):
-    def __init__(self, state_size, action_size, hidden_size=64, learning_rate=1e-4, gamma=0.99, epsilon=0.7,
-                 epsilon_decay=0.995, epsilon_min=0.01, batch_size=64,
-                 replay_buffer_size=10000, tau=0.005):
+    def __init__(self, state_size, action_size, hidden_size=64, learning_rate=1e-3, gamma=0.6, epsilon=0.4,
+                 epsilon_decay=0.9999, epsilon_min=0.1, batch_size=128,
+                 tau=0.005, use_lr_scheduler: bool = False):
         super().__init__()
         self.state_size = state_size
         self.action_size = action_size
@@ -26,8 +28,8 @@ class DQNAgent(TrainableAgent):
         self.epsilon_decay = epsilon_decay
         self.epsilon_min = epsilon_min
         self.batch_size = batch_size
-        self.replay_buffer = ReplayBuffer(replay_buffer_size)
         self.tau = tau
+        self.use_lr_scheduler = use_lr_scheduler
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
         # Q-Networks
@@ -38,6 +40,7 @@ class DQNAgent(TrainableAgent):
 
         self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.learning_rate, amsgrad=True)
         self.loss_fn = nn.SmoothL1Loss()
+        self.lr_scheduler = ExponentialLR(self.optimizer, gamma=0.99999)
 
         self.modules.append(self.policy_net)
         self.modules.append(self.target_net)
@@ -55,8 +58,9 @@ class DQNAgent(TrainableAgent):
             action[:, (~action_mask.astype(bool)).tolist()] = -float('inf')
             return action.argmax().item()
 
-    def train_update(self, replay_buffer: ReplayBuffer):
-        if len(replay_buffer) < self.batch_size:
+    def train_update(self, replay_buffer: ReplayBuffer) -> Optional[Dict[str, Any]]:
+        # TODO: add the return type hinting to other agents as well, and type hinting in general
+        if len(replay_buffer) < self.batch_size * 5:
             return
 
         batch = replay_buffer.sample(self.batch_size)
@@ -70,28 +74,24 @@ class DQNAgent(TrainableAgent):
         rewards = torch.FloatTensor(rewards).to(self.device)
         dones = torch.FloatTensor(dones).to(self.device)
 
-        # Compute Q-values for current states
+        # compute Q-values for current states
         q_values = self.policy_net(states).gather(1, actions).squeeze(1)
 
-        # Compute Q-values for next states using the target network
+        # compute Q-values for next states using the target network
         with torch.no_grad():
             next_action_mask_bool = np.array(next_action_mask).astype(bool)
             all_next_q_values = np.ma.masked_array(self.target_net(next_states), ~next_action_mask_bool)
             next_q_values = torch.tensor(all_next_q_values.max(1))
             target_q_values = rewards + (self.gamma * next_q_values * (1 - dones))
 
-        # Compute loss
         loss = self.loss_fn(q_values, target_q_values)
-        # print(f"Loss: {loss.item()}")
 
-        # Optimize the model
         self.optimizer.zero_grad()
         loss.backward()
 
         torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
         self.optimizer.step()
 
-        # Epsilon decay
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
@@ -101,3 +101,6 @@ class DQNAgent(TrainableAgent):
             target_net_state_dict[key] = (policy_net_state_dict[key] * self.tau +
                                           target_net_state_dict[key] * (1 - self.tau))
         self.target_net.load_state_dict(target_net_state_dict)
+        if self.use_lr_scheduler:
+            self.lr_scheduler.step()
+        return {'loss': loss.item(), 'epsilon': self.epsilon, 'lr': self.lr_scheduler.get_last_lr()[0]}
--- train/train.py
+++ train/train.py
@@ -1,79 +1,90 @@
 import copy
+import datetime
+import logging
 import random
 from collections import deque
 from typing import List, Dict
 
 import numpy as np
+import torch
 from pettingzoo import AECEnv
 from pettingzoo.classic import tictactoe_v3
 from tqdm import tqdm
 
 import agents
+from agents.agent import Agent
+from agents.choose_first_action_agent import ChooseFirstActionAgent
+from agents.hierarchical_agent import HierarchicalAgent
 from agents.random_agent import RandomAgent
 from agents.trainable_agent import TrainableAgent
 from agents.dqn_agent import DQNAgent
+from utils.logger import get_logger
 # from environments import ultimate_ttt
 
 from utils.replay_buffer import ReplayBuffer
 from utils.utils import get_action_mask
 
-def evaluate(env: AECEnv, agent: TrainableAgent, n_games: int = 100, seed: int = 42) -> float:
+
+def evaluate(env: AECEnv, agent: TrainableAgent, n_games: int = 100, seed: int = 42,
+             opponent_agent: Agent = ChooseFirstActionAgent()) -> float:
     """
     evaluate the agent
     :param env: environment to play in
     :param agent: agent to evaluate
     :param n_games: number of episodes to evaluate for
     :param seed: seed for reproducibility
+    :param opponent_agent: opponent to evaluate against
     :return: average reward over the n_games episodes
     """
-    random_agent = RandomAgent()
 
-    env.reset(seed=seed)
-    agent.eval()
-    total_reward = 0
-    for _ in range(n_games):
-        env.reset()
+    with torch.no_grad():
+        env.reset(seed=seed)
+        agent.eval()
+        total_reward = 0
+        for _ in range(n_games):
+            env.reset()
 
-        main_agent_idx = random.randint(0, 1)
-        if main_agent_idx == 0:
-            players = [agent, random_agent]
-        else:
-            players = [random_agent, agent]
-
-        curr_player_idx = 0
-        cumulative_rewards = [0, 0]
-
-        for curr_agent_str in env.agent_iter():
-            curr_player = players[curr_player_idx]
-
-            observation, reward, termination, truncation, info = env.last()
-            done = termination or truncation
-            if done:
-                action = None
+            main_agent_idx = random.randint(0, 1)
+            if main_agent_idx == 0:
+                players = [agent, opponent_agent]
             else:
-                action_mask = get_action_mask(observation, info)
-                action = curr_player.play(env, observation, 0, curr_agent_str, action_mask, info)
+                players = [opponent_agent, agent]
 
-            env.step(action)
-            curr_player_idx = (curr_player_idx + 1) % len(players)
+            curr_player_idx = 0
+            cumulative_rewards = [0, 0]
 
-            cumulative_rewards[curr_player_idx] += reward
+            for curr_agent_str in env.agent_iter():
+                curr_player = players[curr_player_idx]
 
-        is_draw = cumulative_rewards[0] == cumulative_rewards[1]
-        if is_draw:
-            total_reward += 0.5
+                observation, reward, termination, truncation, info = env.last()
+                done = termination or truncation
+                if done:
+                    action = None
+                else:
+                    action_mask = get_action_mask(observation, info)
+                    action = curr_player.play(env, observation, curr_player_idx, curr_agent_str, action_mask, info)
 
-        else:
-            winner = np.argmax(cumulative_rewards).item()
-            if winner == main_agent_idx:
-                total_reward += 1
+                env.step(action)
+                cumulative_rewards[curr_player_idx] += reward
+                curr_player_idx = (curr_player_idx + 1) % len(players)
 
-    return total_reward / n_games
+            is_draw = cumulative_rewards[0] == cumulative_rewards[1]
+            if is_draw:
+                total_reward += 0.5
+
+            else:
+                winner = np.argmax(cumulative_rewards).item()
+                if winner == main_agent_idx:
+                    total_reward += 1
+
+        return total_reward / n_games
 
 
 def add_episode_to_replay_buffer(replay_buffer: ReplayBuffer,
                                  episode_buffer: List[Dict],
-                                 discount_factor: float) -> None:
+                                 discount_factor: float,
+                                 main_agent_idx: int,
+                                 main_agent_only: bool) -> None:
     cumulative_reward = [0., 0.]
     for i in range(len(episode_buffer) - 1, -1, -1):
         curr_record = episode_buffer[i]
@@ -83,11 +94,12 @@ def add_episode_to_replay_buffer(replay_buffer: ReplayBuffer,
         curr_record['cumulative_reward'] = cumulative_reward[curr_player_idx]
 
     for curr_record in episode_buffer:
-        replay_buffer.push(**curr_record)
+        if (not main_agent_only) or curr_record['curr_player_idx'] == main_agent_idx:
+            replay_buffer.push(**curr_record)
 
 
 def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
-          agent_pool_size: int = 20,
+          agent_pool_size: int = 10,
           add_agent_to_pool_every: int = 500,
           train_every: int = 1,
           replay_buffer_size: int = 10_000,
@@ -95,7 +107,9 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
           seed: int = 42,
           render_every: int = 1000,
           renderable_env: AECEnv = None,
-          evaluate_every: int = 50) -> None:
+          evaluate_every: int = 100,
+          only_main_agent_to_replay: bool = False,
+          logger: logging.Logger = None) -> None:
     """
     train using self-play
     :param env: environment to play in
@@ -109,7 +123,10 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
     :param seed: seed for reproducibility
     :param render_every: when to render an episode to screen
     :param renderable_env: same as env, but with render_mode='human'. will be displayed render episodes
+    :param logger: logger object
     """
+    if logger is None:
+        logger = get_logger(__name__)
     assert isinstance(agent, TrainableAgent)
     assert 0 <= discount_factor <= 1
     env.reset(seed=seed)
@@ -118,7 +135,10 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
     replay_buffer = ReplayBuffer(size=replay_buffer_size)
     previous_agents = deque(maxlen=agent_pool_size)
     previous_agents.append(copy.deepcopy(agent))
-    for curr_game in tqdm(range(n_games)):
+    pbar = tqdm(range(n_games))
+    last_eval = None
+    log_dict = {}
+    for curr_game in pbar:
         episode_buffer = []
         main_agent_idx = random.randint(0, 1)
         if main_agent_idx == 0:
@@ -134,7 +154,8 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
         env.reset()
         for curr_agent_str in env.agent_iter():
             if curr_game % train_every == 0:
-                agent.train_update(replay_buffer)
+                train_data = agent.train_update(replay_buffer)
+                log_dict.update({'last_eval': last_eval, **(train_data or {})})
 
             curr_player = players[curr_player_idx]
             observation, reward, termination, truncation, info = env.last()
@@ -157,7 +178,9 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
             if done:
                 action = None
             else:
-                action = curr_player.play(env, observation, curr_player_idx, curr_agent_str, action_mask, info)
+                with torch.no_grad():
+                    action = curr_player.play(env, observation, curr_player_idx, curr_agent_str, action_mask, info)
+
                 episode_buffer.append(dict(observation=observation['observation'],
                                            action=action,
                                            action_mask=action_mask,
@@ -167,6 +190,7 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
             env.step(action)
             curr_player_idx = (curr_player_idx + 1) % len(players)
 
+        log_dict['last_episode_length'] = len(episode_buffer)
         if curr_game > 0 and curr_game % add_agent_to_pool_every == 0:
             previous_agents.append(copy.deepcopy(agent))
 
@@ -174,9 +198,12 @@ def train(env: AECEnv, agent: TrainableAgent, n_games: int = 10_000,
             env = original_env
 
         if curr_game > 0 and curr_game % evaluate_every == 0:
-            print(evaluate(env, agent, n_games=5, seed=seed))
+            last_eval = evaluate(env, agent, n_games=10, seed=seed)
 
-        add_episode_to_replay_buffer(replay_buffer, episode_buffer, discount_factor)
+        add_episode_to_replay_buffer(replay_buffer, episode_buffer, discount_factor,
+                                     main_agent_idx, only_main_agent_to_replay)
+        logger.info(log_dict)
+        pbar.set_postfix(log_dict)
 
     env.close()
 
@@ -188,5 +215,13 @@ if __name__ == '__main__':
     env.reset()
     state_size = env.unwrapped.observation_spaces[env.agents[0]].spaces['observation'].shape
     action_size = env.action_space(env.agents[0]).n
-    agent = DQNAgent(state_size=state_size, action_size=action_size)
-    train(env, agent, n_games=100_000, render_every=20_000, renderable_env=renderable_env)
+    discount_factor = 0.6
+    date_str = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    train_logger = get_logger('train', log_dir_name=f'logs/train_{date_str}')
+
+    agent = DQNAgent(state_size=state_size, action_size=action_size, learning_rate=3e-4,
+                     gamma=discount_factor, use_lr_scheduler=True)
+    train(env, agent, n_games=100_000, render_every=1_000,
+          renderable_env=renderable_env, only_main_agent_to_replay=False,
+          discount_factor=discount_factor,
+          logger=train_logger)
