3d904ac2407c708f26d4c38dfee66c327a0338b6
putskan <ben.lahav101@gmail.com>
2024-08-31 11:44:29 +0300

    add: PrevDQN

--- models/dqn.py
+++ models/dqn.py
@@ -2,12 +2,29 @@ from typing import Sequence, Tuple, Union
 
 import numpy as np
 from torch import nn
+from torch.nn import functional as F
 import torch
+
+
+class PrevDQN(nn.Module):
+    def __init__(self, n_observations, n_actions):
+        super().__init__()
+        n_observations = np.prod(n_observations)
+        self.flatten = nn.Flatten()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
 
 
 class DQN(nn.Module):
     def __init__(self, n_observations, n_actions):
-        super(DQN, self).__init__()
+        super().__init__()
         n_observations = 9  # TODO: change, TODO: add prev version
         self.flatten = nn.Flatten()
         self.net = nn.Sequential(
