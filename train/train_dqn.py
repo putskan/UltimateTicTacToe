3462e425262ede5f013a7ea49dcf60cad8e96f4b
putskan <ben.lahav101@gmail.com>
2024-08-31 11:40:17 +0300

    working dqn conf

--- train/train_dqn.py
+++ train/train_dqn.py
@@ -3,15 +3,19 @@ import datetime
 from pettingzoo.classic import tictactoe_v3
 
 from agents.dqn_agent import DQNAgent
+from environments import ultimate_ttt
 from train import train
 
 if __name__ == '__main__':
     """
     script for training DQN
     """
-    # env = ultimate_ttt.env(render_mode='human', depth=2, render_fps=10)
-    env = tictactoe_v3.env(render_mode=None)
-    renderable_env = tictactoe_v3.env(render_mode='human')
+    # env = tictactoe_v3.env(render_mode=None)
+    # renderable_env = tictactoe_v3.env(render_mode='human')
+
+    env = ultimate_ttt.env(render_mode=None, depth=1)
+    renderable_env = ultimate_ttt.env(render_mode='human', depth=1, render_fps=2)
+
     env.reset()
     state_size = env.unwrapped.observation_spaces[env.agents[0]].spaces['observation'].shape
     action_size = env.action_space(env.agents[0]).n
@@ -24,4 +28,5 @@ if __name__ == '__main__':
           renderable_env=renderable_env, only_main_agent_to_replay=False,
           discount_factor=discount_factor,
           folder_to_save='logs',
+          save_checkpoint_every=5000,
           )
